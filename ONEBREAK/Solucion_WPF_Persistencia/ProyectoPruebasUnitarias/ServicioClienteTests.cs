a38fad29acd7e93514367df2ae1871c2f2868836
nataly <ch.lin@alumnos.duoc.cl>
2020-06-29 14:48:55 -0400

    Pruebas de Clientes parte 1

---
+++ ONEBREAK/Solucion_WPF_Persistencia/ProyectoPruebasUnitarias/ServicioClienteTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.Tests
+{
+    [TestClass()]
+    public class ServicioClienteTests
+    {
+        /// <summary>
+        ///public override void AddEntity(Cliente entity)
+        /// </summary>
+        [TestMethod()]
+        public void AddEntityTest()
+        {
+            Assert.Fail();
+        }
+        //public override void DeleteEntity(object key)
+        [TestMethod()]
+        public void DeleteEntityTest()
+        {
+            Assert.Fail();
+        }
+        // public override void UpdateEntity(Cliente entity)
+        [TestMethod()]
+        public void UpdateEntityTest()
+        {
+            Assert.Fail();
+        }
+
+        
+    }
+}
